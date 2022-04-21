b336c8b4072630c0da80fa1d014744b30d2badd9
oleesir <oleesir@gmail.com>
2022-04-21 02:34:04 +0100

    created the add advert page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import CreateAdvert from "./pages/CreateAdvert";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<LandingPage />
+			<Router>
+				<Header />
+				<Routes>
+					<Route path="/" element={<LandingPage />} />
+					<Route path="/new_advert" element={<CreateAdvert />} />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,34 @@
 import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import Button from "../Button/index.jsx";
 import styles from "./Header.module.css";
 
 const Header = () => {
+	const navigate = useNavigate();
+	let location = useLocation();
+	const navigateToAdvert = () => {
+		navigate("/new_advert");
+	};
+
+	const navigateBack = () => {
+		navigate(-1);
+	};
 	return (
 		<div className={styles.wrapper}>
 			<h1 className={styles.auto}>
 				Auto<span className={styles.motor}>mobile</span>
 			</h1>
-			<Button btnTypes="SellBtn" type="button">
-				Sell
-			</Button>
+			{location.pathname === "/" && (
+				<Button btnTypes="SellBtn" type="button" onClick={navigateToAdvert}>
+					Sell
+				</Button>
+			)}
+
+			{location.pathname === "/new_advert" && (
+				<Button btnTypes="SellBtn" type="button" onClick={navigateBack}>
+					Back
+				</Button>
+			)}
 		</div>
 	);
 };
